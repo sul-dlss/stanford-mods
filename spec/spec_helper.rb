60b6211eca3d00f57d66654a2300492ba6ddbb04
Naomi Dushay <ndushay@stanford.edu>
2014-05-19 15:20:34 -0700

    remove pre-coveralls simplecov from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,3 @@
-# for test coverage
-=begin 
-require 'simplecov'
-require 'simplecov-rcov'
-class SimpleCov::Formatter::MergedFormatter
-  def format(result)
-     SimpleCov::Formatter::HTMLFormatter.new.format(result)
-     SimpleCov::Formatter::RcovFormatter.new.format(result)
-  end
-end
-
-SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
-SimpleCov.start do
-  add_filter "/spec/"
-end
-=end
 require 'coveralls'
 Coveralls.wear!
 
