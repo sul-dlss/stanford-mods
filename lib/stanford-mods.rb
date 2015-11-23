98116b825ebf63364ec82fadb3a9032acb028ed5
Laney McGlohon <laneymcg@stanford.edu>
2015-11-23 10:45:22 -0800

    Move coordinates and point bounding boxes from spotlight-dor-resources

--- lib/stanford-mods.rb
+++ lib/stanford-mods.rb
@@ -3,6 +3,7 @@ require 'mods'
 require 'stanford-mods/name'
 require 'stanford-mods/searchworks'
 require 'stanford-mods/physical_location'
+require 'stanford-mods/geo_spatial'
 
 # Stanford specific wranglings of MODS metadata as an extension of the Mods::Record object
 module Stanford
