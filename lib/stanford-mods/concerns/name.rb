b053e08675f3c5d8934d0c33a8bcb64208e919d1
Chris Beer <chris@cbeer.info>
2022-01-27 08:15:45 -0800

    Move collector + non-collector methods into the exhibits indexer, their only consumer

--- lib/stanford-mods/concerns/name.rb
+++ lib/stanford-mods/concerns/name.rb
@@ -65,27 +65,6 @@ module Stanford
       def additional_authors_w_dates
         mods_ng_xml.plain_name.map(&:display_value_w_date) - [main_author_w_date]
       end
-
-      # @return Array of Strings, each containing the computed display value of a personal name
-      #   except for the collector role (see mods gem nom_terminology for display value algorithm)
-      def non_collector_person_authors
-        mods_ng_xml.personal_name.select { |n| n.role.any? }.reject { |n| n.role.all? { |r| includes_marc_relator_collector_role?(r) } }.map(&:display_value_w_date)
-      end
-
-      # @return Array of Strings, each containing the computed display value of
-      #  a personal name with the role of Collector (see mods gem nom_terminology for display value algorithm)
-      def collectors_w_dates
-        mods_ng_xml.personal_name.select { |n| n.role.any? }.select { |n| n.role.any? { |r| includes_marc_relator_collector_role?(r) } }.map(&:display_value_w_date)
-      end
-
-      COLLECTOR_ROLE_URI = 'http://id.loc.gov/vocabulary/relators/col'.freeze
-
-      # @param Nokogiri::XML::Node role_node the role node from a parent name node
-      # @return true if there is a MARC relator collector role assigned
-      def includes_marc_relator_collector_role?(role_node)
-        (role_node.authority.include?('marcrelator') && role_node.value.include?('Collector')) ||
-        role_node.roleTerm.valueURI.first == COLLECTOR_ROLE_URI
-      end
     end # class Record
   end # Module Mods
 end # Module Stanford
